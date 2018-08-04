c0033399556c0b1e53db00353e35cabc76e3c09f
Shadowlight2017 <39026536+Shadowlight2017@users.noreply.github.com>
2018-08-04 20:18:26 +0200

    Prototyp 0.1 (Movement)

---
+++ ActionRPGTutorial_2/Assets/Scripts/GlobalPlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalPlayerController : MonoBehaviour
+{
+    
+    int player_ID = 0;
+    GameObject selectedPlayer;
+    public List<GameObject> players = new List<GameObject>()
+        {
+            /*GameObject.Find("Player_1"),
+            GameObject.Find("Player_2"),
+            GameObject.Find("Player_3")*/
+        };
+
+    public void DisableBoolean(params bool[] enabled)
+    {
+        players[0].GetComponent<PlayerController>().enabled = enabled[0];
+        players[1].GetComponent<PlayerController>().enabled = enabled[1];
+        players[2].GetComponent<PlayerController>().enabled = enabled[2];
+    }
+
+    void Start()
+    {
+        selectedPlayer = players[0];
+    }
+
+    void Update()
+    {
+        selectedPlayer.GetComponent<PlayerController>().moveHorizontal = Input.GetAxisRaw("Horizontal");
+        selectedPlayer.GetComponent<PlayerController>().moveVertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            selectedPlayer.GetComponent<PlayerController>().moveHorizontal = 0;
+            selectedPlayer.GetComponent<PlayerController>().moveVertical = 0;
+            selectedPlayer.GetComponent<Animator>().SetFloat("MoveX", 0);
+            selectedPlayer.GetComponent<Animator>().SetFloat("MoveY", 0);
+
+            if (player_ID == players.Count -1)
+            {
+                player_ID = 0;
+                Debug.LogFormat("ID has been reset/set to 0");
+            }
+            else
+            {
+                player_ID++;
+                Debug.LogFormat("ID has been changed");
+            }
+
+            foreach (GameObject i in players)
+            {
+                if (player_ID == i.GetComponent<PlayerController>().characterID)
+                {
+                    selectedPlayer = i;
+                }
+            }
+        }
+    }
+}
---
+++ ActionRPGTutorial_2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float moveHorizontal;
+    public float moveVertical;
+    public bool playerMoving;
+    public Vector2 lastMove;
+    private Animator anim;
+    public int characterID;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    void Update(){
+
+        playerMoving = false;
+
+        
+
+        if (moveHorizontal != 0)
+        {
+            
+            lastMove = new Vector2(moveHorizontal, 0f);
+            playerMoving = true;
+        }
+
+        
+
+        if (moveVertical != 0)
+        {
+            
+            lastMove = new Vector2(0f, moveVertical);
+            playerMoving = true;
+        }
+
+        anim.SetFloat("MoveX", moveHorizontal);
+        anim.SetFloat("MoveY", moveVertical);
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+
+    void FixedUpdate () {
+        transform.Translate(new Vector3(moveHorizontal * Time.deltaTime * moveSpeed, moveVertical * Time.deltaTime * moveSpeed));
+        playerMoving = true;
+        
+    }
+}
